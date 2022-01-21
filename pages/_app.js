12ea98faa10f81e56650b4170616239739bdbbdf
Oscar Barajas Tavares <oscar@gndx.dev>
2022-01-21 09:51:16 -0500

    :sparkles: [NEW] Initial Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import { Web3ReactProvider } from "@web3-react/core";
+import { getLibrary } from "../config/web3";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Component {...pageProps} />
+    </Web3ReactProvider>)
 }
 
 export default MyApp
